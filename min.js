48e1c9a85959a80db1be73ef53f023abbb13fb59
Abdullah135792 <abd0272829a@gmail.com>
2025-03-19 23:06:48 +0200

    Add files via upload

---
+++ min.js
@@ -0,0 +1,27 @@
+let il=document.getElementsByTagName('i')
+let p=document.getElementsByTagName('p')
+console.log(22);
+
+for(let pp of p ){
+    function hh(u){
+
+        console.log(u);
+    }
+// for(let ii of il ){
+//     ii.onmouseenter=function(){
+
+
+        
+        
+        
+//     }
+//     ii.addEventListener('click',function(){
+// let nn =ii.firstElementChild;
+// console.log();
+
+
+//     })
+    
+}
+
+//<i class="fa-solid fa-cart-shopping" style="color: #cd1539;">
